ae6392d2f7b65ecbb71e8bbea905b5dffe09cb64
Dinesh10731j <dinesh1122@gmail.com>
2024-06-17 00:27:11 +0545

    done

--- model/flux.js
+++ model/flux.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
+
+
+
 mongoose
   .connect(process.env.MONGO_URL)
   .then((data) => console.log("Database connectd successfully"))
