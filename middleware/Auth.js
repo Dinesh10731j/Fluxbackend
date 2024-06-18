8ec0ea3ccc342ff6dfe98ad448b7eb6001a91c04
Dinesh10731j <dinesh1122@gmail.com>
2024-06-19 05:42:47 +0545

    done

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -1,5 +1,19 @@
+const jwt = require("jsonwebtoken")
 const CheckToken =(req,res,next)=>{
 
-    
+const token = req.headers['Authorization'][0];
+
+const verifiedToken = jwt.verify(token,process.env.Signature);
+
+if(verifiedToken){
+    res.send({msg:"Authorized Token"});
+    next();
+}else{
+    res.status(401).send({msg:"Unauthorized"});
+}
 
 }
+
+
+
+module.exports=CheckToken;
